5ecd4bf1a428fd2c93faf4fc6ff56288717a06fb
Rena Jair <164107426+Rene523@users.noreply.github.com>
2025-01-22 23:10:18 +0000

    Final Commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,9 +5,9 @@ public class SelectionSort implements Sorter {
         for(int i =0; i<input.length;i++){
             int min = input[i];
             for(int k =0; k<input.length;k++){
-                if(min>input[k]){
-                    input[i]=input[k];
+                if(min<input[k]){
                     input[k]=input[i];
+                    input[i]=input[k];
                 }
             }
         }

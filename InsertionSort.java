9e1342b4ad909feaf36529e9b83f5b9f69c96725
Rena Jair <164107426+Rene523@users.noreply.github.com>
2025-01-22 22:58:36 +0000

    draft

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,5 +2,14 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        for(int i=0;i<input.length; i++){
+            int num = input[i];
+            for(int k=0;k<i; k++){
+                if(num<input[k]){
+                    input[i]=input[k];
+                    input[k]=input[i];
+                }
+            }
+        }
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -2,5 +2,14 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        for(int i =0; i<input.length;i++){
+            int min = input[i];
+            for(int k =0; k<input.length;k++){
+                if(min>input[k]){
+                    input[i]=input[k];
+                    input[k]=input[i];
+                }
+            }
+        }
     }
 }
